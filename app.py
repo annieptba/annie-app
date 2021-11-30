03638a07f6d30a059a56bfc004fe2a14fc7851d7
annieptba <61258785+annieptba@users.noreply.github.com>
2021-11-30 03:14:53 -0500

    qwe

--- app.py
+++ app.py
@@ -1,29 +1,22 @@
-import os
-
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc # dash core components
+from dash import html
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__)
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-server = app.server
-
-app.layout = html.Div([
-    html.H2('Hello World'),
-    dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
-    ),
-    html.Div(id='display-value')
+app.layout = html.Div(children=[
+    html.H2(children='Greetings!'),
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [5, 7, 2], 'type': 'line'},
+            ],
+            'layout': {
+                'title': 'Example Graph'
+            }
+        }
+    )
 ])
 
-@app.callback(dash.dependencies.Output('display-value', 'children'),
-                [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
+app.run_server(debug=True, host="0.0.0.0")
